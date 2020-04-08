2fd7f3e4a249410df22d288451c7760a824cb768
edwardmartinsjr <edwardmartinsjr@gmail.com>
2020-04-08 21:57:52 +0200

    add get repos function

--- db.sql
+++ db.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 DROP TABLE IF EXISTS organizations;
+DROP TABLE IF EXISTS repos;
 
 CREATE TABLE IF NOT EXISTS organizations(
     id INTEGER NOT NULL,
@@ -8,8 +9,14 @@ CREATE TABLE IF NOT EXISTS organizations(
     CONSTRAINT pk_organizations PRIMARY KEY (id)
 );
 
-COMMIT;
+CREATE TABLE IF NOT EXISTS repos(
+    git_url TEXT NOT NULL,
+    language TEXT NOT NULL,
+    archived BOOLEAN NOT NULL,
+    forks_count INT NOT NULL,
+    open_issues_count INT NOT NULL,
+    watchers_count INT NOT NULL,
+    CONSTRAINT pk_repos PRIMARY KEY (git_url)
+);
 
--- SELECT count(*) FROM organizations;
--- SELECT id FROM organizations ORDER BY 1 DESC LIMIT 1;
--- SELECT * FROM organizations;
+COMMIT;
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pandas as pd
 import requests
 import store
 
-get_organizations_url = "https://api.github.com/organizations?since="
+get_organizations_url = 'https://api.github.com/organizations?since='
+get_repos_url = 'https://api.github.com/orgs/%(login)s/repos'
 
 headers = {'Content-Type': 'application/json'}
 
 def get_organizations(organization_id):
-    organizations = pd.DataFrame(columns = ['id', 'login'])    
+    organizations_columns = ['id', 'login']
+    organizations = pd.DataFrame(columns = organizations_columns)    
 
     # Extract the organizations JSON string to pandas object (since pagination).
     resp = requests.get(get_organizations_url + str(organization_id), headers = headers)
@@ -17,22 +19,59 @@ def get_organizations(organization_id):
         pass
     elif resp.status_code == 200:
         # Read data
-        organizations=pd.read_json(resp.content)
+        organizations=pd.read_json(resp.content)[organizations_columns]
     else:
         print(resp.content)
         pass
 
     # Transform data
     organizations['id'] = pd.to_numeric(organizations['id'])
+    organizations['login'] = organizations['login'].astype('str')
 
     return organizations
+
+def get_repos(organizations):
+    repos_columns = ['git_url', 'language', 'archived', 'forks_count', 'open_issues_count', 'watchers_count']
+    repos = pd.DataFrame(columns = repos_columns)    
+
+    # Extract the organizations repos JSON string to pandas object.
+    for row in organizations.itertuples():
+        resp = requests.get(get_repos_url % {'login': row.login}, headers = headers)
+        
+        if resp.status_code == 403:
+            print('API rate limit exceeded')
+            break
+        elif resp.status_code == 200:
+            # Read data
+            if len(resp.json()) > 0:
+                repos = repos.append(pd.read_json(resp.content)[repos_columns])
+        else:
+            print(resp.content)
+            pass
+
+    # Transform data
+    repos['git_url'] = repos['git_url'].astype('str')
+    repos['language'] = repos['language'].astype('str')
+    repos['forks_count'] = pd.to_numeric(repos['forks_count'])
+    repos['open_issues_count'] = pd.to_numeric(repos['open_issues_count'])
+    repos['watchers_count'] = pd.to_numeric(repos['watchers_count'])
+    repos['archived'] = repos['archived'].astype('bool')
+
+    return repos
+
             
 if __name__== '__main__':
     # Get organizations
     organizations = get_organizations(store.get_last_organization_id())
 
     # Store organizations
-    store.save_data(organizations[['id','login']], 'organizations')
+    store.save_data(organizations, 'organizations')
+
+    # Get repos
+    repos = get_repos(organizations)
+
+    # Store repos
+    store.save_data(repos, 'repos')
 
 
     
